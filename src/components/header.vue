d8732c9ae5076a7e3265d0c6c6e69739ad78c635
johnFarkasolo <farkasolo@mail.ru>
2018-05-27 15:33:55 +0200

    hot fix

--- src/components/header.vue
+++ src/components/header.vue
@@ -12,27 +12,27 @@
 #backGround{
 	height: 100vh;
 	width: 100%;
-	background-image: url('../assets/canvas.png');
-	background-size: cover;
+	/* background-image: url('../assets/canvas.png'); */
+	/* background-size: cover; */
 	background: linear-gradient(319deg, #bc00ff, #4c0fcc);
 	background-size: 400% 400%;
 
--webkit-animation: AnimationName 5s ease infinite;
--moz-animation: AnimationName 5s ease infinite;
-animation: AnimationName 5s ease infinite;
+-webkit-animation: Gradient 6s ease infinite;
+-moz-animation: Gradient 6s ease infinite;
+animation: Gradient 6s ease infinite;
 }
 
- @-webkit-keyframes AnimationName {
+ @-webkit-keyframes Gradient {
     0%{background-position:87% 0%}
     50%{background-position:14% 100%}
     100%{background-position:87% 0%}
 }
-@-moz-keyframes AnimationName {
+@-moz-keyframes Gradient {
     0%{background-position:87% 0%}
     50%{background-position:14% 100%}
     100%{background-position:87% 0%}
 }
-@keyframes AnimationName {
+@keyframes Gradient {
     0%{background-position:87% 0%}
     50%{background-position:14% 100%}
     100%{background-position:87% 0%}
