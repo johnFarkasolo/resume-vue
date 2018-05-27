1e232780e281632df50fa1b094037f8ab850e6ea
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-05-27 15:27:30 +0200

    Added main element. Renamed header -> intro

--- src/components/intro.vue
+++ src/components/intro.vue
@@ -4,7 +4,7 @@
 
 <script>
     export default {
-        name: 'it Header',
+        name: 'it Intro',
     }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Header from '../components/header';
+import Header from '../components/intro';
 
 Vue.use(Router);
 
