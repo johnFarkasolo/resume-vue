105ec61834b3505567671b1d609f8474af5f51e2
johnFarkasolo <farkasolo@mail.ru>
2018-05-27 12:32:37 +0200

    background animation

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,33 @@
 <template>
-      <p>{{ name }}</p>
+			<div id="backGround"></div>
 </template>
 
 <script>
     export default {
-        name: 'it Header'
+        name: 'it Header',
     }
 </script>
 
 <style scoped>
+#backGround{
+	height: 400px;
+	width:100%;
+	background-image: url('../assets/canvas.png');
+	background-size: cover;
+	animation: animatedBackground 60s linear infinite;
+	-moz-animation: animatedBackground 60s linear infinite;
+	-webkit-animation: animatedBackground 60s linear infinite;
+ 	-o-animation: animatedBackground 60s linear infinite;
+}
+
+@keyframes animatedBackground {
+    0 {
+        background-position: 0 0
+    }
+    100% {
+        background-position: -600px 600px
+    }
+}
+
 
 </style>
