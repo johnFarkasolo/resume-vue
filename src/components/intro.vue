071fc9711edb93e1fa288e91d888334d6f20f66c
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-05-28 21:36:22 +0200

    Basic content to work on added

--- src/components/intro.vue
+++ src/components/intro.vue
@@ -1,41 +1,79 @@
 <template>
-				<parallax>
-					<div class="intro-body">
-						<a href="" class="intro-logo"><img src="../assets/logo.png" alt=""></a>
-					</div>
-				</parallax>
+  <parallax>
+    <div class="intro-body">
+      <a href="" class="intro-logo"><img src="../assets/logo.png" alt=""></a>
+      <div id="photo">
+        <img src="../assets/photo/tony.jpg" alt="">
+      </div>
+      <div id="name">
+        <h1>Robert Downey Jr.</h1>
+      </div>
+      <div id="passion">
+        <h2>FRONT-END WEB DEVELOPER</h2>
+      </div>
+      <div id="about">
+        <ul>
+          <li>I like to code because I really enjoy building things (from "lego" to chair to websites and JS-apps)</li>
+          <li>And I like to be able to take an idea and make it happen on the screen</li>
+          <li>Coding is my passion because coding is the way of the future</li>
+          <li>And I try to make this world a better place</li>
+          <li>And may I tell you a secret? I have a dream to get a job with a good pay )))</li>
+        </ul>
+      </div>
+    </div>
+  </parallax>
 </template>
 
 <script>
-    export default {
-        name: 'it Intro',
-    }
+  export default {
+    name: 'it Intro',
+  }
 </script>
 
 <style scoped>
-.intro-body{
-	height: 100vh;
-	width: 100%;
-	background: linear-gradient(180deg, #bc00ff, #4c0fcc);
-	background-size: 300% 300%;
-	animation: introGradient 8s ease infinite;
-		-webkit-animation: introGradient 8s ease infinite;
-		-moz-animation: introGradient 8s ease infinite;
-}
+  * {
+    color: cornsilk;
+  }
 
-@keyframes introGradient {
-    0%{background-position:87% 0%}
-    50%{background-position:14% 100%}
-    100%{background-position:87% 0%}
-}
-.intro-logo {
-		margin-top: 50px;
-		position: fixed;	
-		box-sizing: border-box;
-		z-index: 999;
-	}
-	.intro-logo img{
-		width: 50px;
-		background: transparent;
-	}
+  .intro-body {
+    height: 100vh;
+    width: 100%;
+    background: linear-gradient(180deg, #bc00ff, #4c0fcc);
+    background-size: 300% 300%;
+    animation: introGradient 8s ease infinite;
+    -webkit-animation: introGradient 8s ease infinite;
+    -moz-animation: introGradient 8s ease infinite;
+  }
+
+  @keyframes introGradient {
+    0% {
+      background-position: 87% 0%
+    }
+    50% {
+      background-position: 14% 100%
+    }
+    100% {
+      background-position: 87% 0%
+    }
+  }
+
+  .intro-logo {
+    margin-top: 50px;
+    position: fixed;
+    box-sizing: border-box;
+    z-index: 999;
+  }
+
+  .intro-logo img {
+    width: 50px;
+    background: transparent;
+  }
+
+  #photo > img {
+    width: 200px;
+  }
+
+  #about > ul {
+    list-style-type: none;
+  }
 </style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,44 +1,112 @@
 <template>
-	<div class="main-body">
-			<parallax>
-				<div id="skills">
-					
-				</div>
-			</parallax>
-			<parallax>
-    		<div id="projects">
-						
-				</div>
-			</parallax>
-			<parallax>
-    		<div id="contacts">
-						
-				</div>
-			</parallax>
-	</div>		
+  <div class="main-body">
+    <parallax>
+      <div id="skills">
+        <h1>
+          Skills
+        </h1>
+        <div id="summary_skills">
+          <h2>
+            SUMMARY OF SKILLS:
+          </h2>
+          <ul>
+            <li>Front-end Developer with X years of experience designing, developing, and managing complex e-commerce
+              sites. Specializes in JS and responsive design
+            </li>
+            <li>Intermediate English level (written and spoken)</li>
+            <li>Strong understanding of industry trends and content management systems</li>
+            <li>Ability to improvise, learn new skills in short terms, self-motivated and result-oriented</li>
+          </ul>
+        </div>
+        <div>
+          <h2>TECHNICAL SKILLS:</h2>
+          <ul>
+            <li>Front-end: CSS3, HTML5, XML, web standards and accessibility, cross-browser compatibility, JSON with
+              padding
+            </li>
+            <li>Programming/scripting languages: JavaScript (ES6, jQuery, Bootstrap), basic knowledge of SQL</li>
+            <li>DB: Oracle</li>
+            <li>Development tools: WebStorm, Note++, Sublime Text, Photoshop/Fireworks, GitHub Desktop</li>
+            <li>APIs: Web Storage API, JSON API, Web-Storage API, Web Workers API, Geolocation API, Google Maps
+              JavaScript API
+            </li>
+            <li>Version control systems: Git</li>
+          </ul>
+        </div>
+      </div>
+    </parallax>
+    <parallax>
+      <!--Это лучше сделать галереей-->
+      <div id="projects" class="container-fluid">
+        <div class="row">
+          <div>
+            <img src="../assets/projects/1.jpg" alt="">
+          </div>
+          <div>
+            <img src="../assets/projects/1.jpg" alt="">
+          </div>
+          <div>
+            <img src="../assets/projects/1.jpg" alt="">
+          </div>
+          <div>
+            <img src="../assets/projects/1.jpg" alt="">
+          </div>
+          <div>
+            <img src="../assets/projects/1.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </parallax>
+    <parallax>
+      <div id="contacts">
+        <ul>
+          <li>tel. +48 ********* Cell Phone</li>
+          <li>Viber +48 ********** Viber</li>
+          <li>Skype *********</li>
+          <li>email ********.com</li>
+          <li>Github www.github.com/*******</li>
+          <li>facebook www.facebook.com/********</li>
+          <li>location **************</li>
+        </ul>
+      </div>
+    </parallax>
+  </div>
 </template>
 
 <script>
   export default {
     name: "main"
-	}
+  }
 </script>
 
 <style scoped>
-	#skills {
-		height: 100vh;
-		width: 100%;
-		background: linear-gradient(240deg, #f5925d, #361c83);
-	}
+  * {
+    color: cornsilk;
+  }
 
-	#projects {
-		height: 100vh;
-		width: 100%;
-		background: linear-gradient(230deg, #78139e, #2f0ba8);
-	}
-	#contacts {
-		height: 100vh;
-		width: 100%;
-		background: linear-gradient(240deg, #5cddd9, #ee4bff);
-	}
+  #skills {
+    height: 100vh;
+    width: 100%;
+    background: linear-gradient(240deg, #f5925d, #361c83);
+  }
+
+  #projects {
+    height: 100vh;
+    width: 100%;
+    background: linear-gradient(230deg, #78139e, #2f0ba8);
+  }
+
+  #contacts {
+    height: 100vh;
+    width: 100%;
+    background: linear-gradient(240deg, #5cddd9, #ee4bff);
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  div > img {
+    width: 300px;
+  }
 </style>
